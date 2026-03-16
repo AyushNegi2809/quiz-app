ccf1ebe6c189700d4af03dcb364b427c3cd5d872
Ayush <ayushnegi912@gmail.com>
2026-03-16 19:10:45 +0530

    npm install tailwindcss for frontend

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./static/js/**/*.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
